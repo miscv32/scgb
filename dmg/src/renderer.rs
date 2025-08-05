e8bc8c26a2c8d87bd49e98b7f49fa8f396164420
Jadon Mensah <35032203+jadonmensah@users.noreply.github.com>
2025-08-05 16:26:02 +0100

    fix background 8800 method bug

--- dmg/src/renderer.rs
+++ dmg/src/renderer.rs
@@ -82,18 +82,158 @@ impl GameBoy {
         }
     }
 
-    fn render_background(&mut self) {
-        let tilemap: u16 = match self.r.lcdc & 8 {
-            0 => 0x9800,
-            _ => 0x9c00,
-        };
-        for x in 0..160 {
-            let tile_x = (x + self.r.scx) % 8;
-            let tile_y = (self.r.ly as u16 + self.r.scy as u16) % 8;
+    // fn render_background(&mut self) {
+    //     let tilemap: u16 = match self.r.lcdc & 8 {
+    //         0 => 0x9800,
+    //         _ => 0x9c00,
+    //     };
+    //     for x in 0..160 {
+    //         let tile_x = (x + self.r.scx) % 8;
+    //         let tile_y = (self.r.ly as u16 + self.r.scy as u16) % 8;
 
-            let x_off: u16 = ((x as u16 + self.r.scx as u16) / 8) & 0x1f;
+    //         let x_off: u16 = ((x as u16 + self.r.scx as u16) / 8) & 0x1f;
+    //         let y_off: u16 = 32 * (((self.r.ly as u16 + self.r.scy as u16) & 0xFF) / 8);
+    //         let tile_num_addr = tilemap + ((x_off + y_off) & 0x3ff);
+    //         let tile_num = self.read(tile_num_addr);
+
+    //         let mut tile_addr: u16;
+    //         match self.r.lcdc & 0x10 {
+    //             0 => {
+    //                 tile_addr = (0x9000 + (tile_num as i32 * 16)) as u16;
+    //             }
+    //             _ => {
+    //                 tile_addr = 0x8000 + (tile_num as u16) * 16;
+    //             }
+    //         };
+
+    //         tile_addr += 2 * tile_y;
+
+    //         let data_low = (self.read(tile_addr) >> (7 - tile_x)) & 1;
+    //         let data_high = (self.read(tile_addr + 1) >> (7 - tile_x)) & 1;
+            
+    //         let ly = self.r.ly;
+            
+    //         let index = ly as usize * 160 + x as usize;
+    //         if index < 160 * 140 {
+    //             self.backbuf()[index] =
+    //                 self.map_background_palette(data_low | (data_high << 1));
+    //             self.background[ly as usize * 160 + x as usize] = self.backbuf()[ly as usize * 160 + x as usize];
+    //         }
+    //     }
+    // }
+
+    // fn scan_oam(&mut self) -> Vec<Sprite>{
+    //     self.oam_base = 0xFE00;
+    //     self.num_sprites = 0;
+    //     let mut sprite_buffer = vec![];
+    //     for _ in 0..40 {
+    //         let sprite = Sprite {
+    //             size_y: (((self.r.lcdc >> 2) & 1) + 1) * 8,
+    //             x: self.oam(1) as i16 - 8,
+    //             y: self.oam(0) as i16 - 16,
+    //             x_flip: (self.oam(3) >> 5) & 1,
+    //             y_flip: (self.oam(3) >> 6) & 1,
+    //             pal: (self.oam(3) >> 4) & 1,
+    //             priority: (self.oam(3) >> 7) & 1,
+    //             tile_num: self.oam(2),
+    //         };
+    //         if (sprite.x > 0) && (self.r.ly as i16>= sprite.y) && ((self.r.ly as i16)< sprite.y + sprite.size_y as i16) && (sprite_buffer.len() < 10) {
+    //             sprite_buffer.push(sprite);
+    //         } 
+    //         self.oam_base += 4;
+    //     }
+    //     sprite_buffer
+    // }
+
+    // fn render_sprite(&mut self, sprite: Sprite) {
+    //     let mut tile_y = ((self.r.ly as i16 + sprite.y) % 8) as u16;
+    //     if sprite.y_flip != 0 {tile_y = 7 - tile_y}
+
+    //     let tile_num = sprite.tile_num;
+
+    //     let mut tile_addr: u16 = 0x8000 + (tile_num as u16) * 16;
+
+    //     tile_addr += 2 * tile_y;
+
+    //     for x in sprite.x..sprite.x+8 {    
+    //         let mut tile_x = x as u16 - sprite.x as u16;
+    //         if sprite.x_flip != 0 {tile_x = 7 - tile_x}
+    //         let shift = 7 - tile_x;
+
+    //         let data_low = (self.read(tile_addr) >> shift) & 1;
+    //         let data_high = (self.read(tile_addr + 1) >> shift) & 1;
+    //         let index = (self.r.ly) as usize * 160 + x as usize;
+    //         let should_draw = true; // TODO add some semi-accurate condition here
+    //         if should_draw {
+    //             self.backbuf()[index] =
+    //                 self.map_sprite_palette(sprite.pal, data_low | (data_high << 1));
+    //         }
+    //     }
+    
+    // }
+
+    fn render_background(&mut self) {
+        let tilemap_base_addr: u16 = match (self.r.lcdc >> 3) & 1 {
+            0 => 0x9800,
+            1 => 0x9C00,
+            _ => unreachable!(),
+        };
+
+        for screen_x in 0..160 {
+            let tile_y = (self.r.ly + self.r.scy) % 8;
+
+            let x_off: u16 = ((screen_x as u16 + self.r.scx as u16) / 8) & 0x1F;
             let y_off: u16 = 32 * (((self.r.ly as u16 + self.r.scy as u16) & 0xFF) / 8);
-            let tile_num_addr = tilemap + ((x_off + y_off) & 0x3ff);
+            let tile_num_addr = tilemap_base_addr + ((x_off + y_off) & 0x3ff);
+            let mut tile_num = self.read(tile_num_addr);
+
+            let mut tile_addr: u16;
+            let tile_data_base;
+            match self.r.lcdc & 0x10 {
+                0 => {
+                    if tile_num <= 127 {
+                        tile_data_base = 0x9000;
+                    } else {
+                        tile_data_base = 0x8800;
+                        tile_num -= 128;
+                    }
+                }
+                _ => {
+                    tile_data_base = 0x8000;
+                }
+            };
+            tile_addr = tile_data_base + (tile_num as u16) * 16;
+
+            tile_addr += 2 * tile_y as u16;
+
+            let tile_data_low = self.read(tile_addr);
+            let tile_data_high = self.read(tile_addr + 1);
+            
+
+            let tile_x = (screen_x + self.r.scx) % 8;
+                
+            let pixel_data_low = (tile_data_low >> (7 - tile_x)) & 1;
+            let pixel_data_high = (tile_data_high >> (7 - tile_x)) & 1;
+                
+            let index: usize = self.r.ly as usize * 160 + screen_x as usize;
+
+            if index < 160 * 144 {
+                self.backbuf()[index] = self.map_background_palette(pixel_data_low | (pixel_data_high << 1))
+            }
+        }
+    }
+    
+    fn render_window(&mut self) { // I think i'm fetching the corect tile data but drawing it to the wrong place
+        let tilemap_base_addr: u16 = match (self.r.lcdc >> 6) & 1 {
+            0 => 0x9800,
+            1 => 0x9C00,
+            _ => unreachable!(),
+        };
+
+        for screen_x in 0..160 {
+            let x_off: u16 = ((screen_x as u16) / 8) & 0x1f;
+            let y_off: u16 = 32 * ((self.window_line_counter as u16 ) / 8);
+            let tile_num_addr = tilemap_base_addr + ((x_off + y_off) & 0x3ff);
             let tile_num = self.read(tile_num_addr);
 
             let mut tile_addr: u16;
@@ -106,124 +246,53 @@ impl GameBoy {
                 }
             };
 
-            tile_addr += 2 * tile_y;
+            tile_addr += 2 * (self.window_line_counter as u16 % 8);
 
-            let data_low = (self.read(tile_addr) >> (7 - tile_x)) & 1;
-            let data_high = (self.read(tile_addr + 1) >> (7 - tile_x)) & 1;
+            let tile_data_low = self.read(tile_addr);
+            let tile_data_high = self.read(tile_addr + 1);
             
-            let ly = self.r.ly;
-            
-            let index = ly as usize * 160 + x as usize;
-            if index < 160 * 140 {
-                self.backbuf()[index] =
-                    self.map_background_palette(data_low | (data_high << 1));
-                self.background[ly as usize * 160 + x as usize] = self.backbuf()[ly as usize * 160 + x as usize];
+
+            let tile_x = (screen_x) % 8;
+                
+            let pixel_data_low = (tile_data_low >> (7 - tile_x)) & 1;
+            let pixel_data_high = (tile_data_high >> (7 - tile_x)) & 1;
+                
+            let index: usize = self.r.ly as u8 as usize * 160 + screen_x as usize;
+
+            if index < 160 * 144 {
+                self.backbuf()[index] = self.map_background_palette(pixel_data_low | (pixel_data_high << 1))
             }
         }
+        self.window_line_counter += 1;
     }
-
-    fn render_window(&mut self) {
-        let tilemap: u16 = match (self.r.lcdc >> 6) & 1 {
-            0 => 0x9800,
-            _ => 0x9c00,
-        };
-        
-        for x in 0..160 {
-            let x_plus_scroll = (x as u16) % 256;
-            let y_plus_scroll = (self.window_line_counter as u16) % 256;
-            let tile_x = x_plus_scroll % 8;
-            let tile_y = y_plus_scroll % 8;
-
-            let tile_num = self.read(tilemap + x_plus_scroll / 8 + (y_plus_scroll / 8) * 32);
-
-            let mut tile_addr: u16;
-            match self.r.lcdc & 0x10 {
-                0 => {
-                    tile_addr = (0x9000 + (tile_num as i32 * 16)) as u16;
-                }
-                _ => {
-                    tile_addr = 0x8000 + (tile_num as u16) * 16;
-                }
-            };
-
-            tile_addr += 2 * tile_y;
-            let data_low = (self.read(tile_addr) >> (7 - tile_x)) & 1;
-            let data_high = (self.read(tile_addr + 1) >> (7 - tile_x)) & 1;
-            let ly = self.r.ly;
-            self.backbuf()[ly as usize * 160 + x as usize] =
-                self.map_background_palette(data_low | (data_high << 1));
-            self.window_line_counter += 1;
-        }
-    }
-
-    fn scan_oam(&mut self) -> Vec<Sprite>{
-        self.oam_base = 0xFE00;
-        self.num_sprites = 0;
-        let mut sprite_buffer = vec![];
-        for _ in 0..40 {
-            let sprite = Sprite {
-                size_y: (((self.r.lcdc >> 2) & 1) + 1) * 8,
-                x: self.oam(1) as i16 - 8,
-                y: self.oam(0) as i16,
-                x_flip: (self.oam(3) >> 5) & 1,
-                y_flip: (self.oam(3) >> 6) & 1,
-                pal: (self.oam(3) >> 4) & 1,
-                priority: (self.oam(3) >> 7) & 1,
-                tile_num: self.oam(2),
-            };
-            if (sprite.x > 0) && (self.r.ly as i16 + 16 >= sprite.y) && (self.r.ly as i16 + 16 < sprite.y + sprite.size_y as i16) && (sprite_buffer.len() < 10) {
-                sprite_buffer.push(sprite);
-            } 
-            self.oam_base += 4;
-        }
-        sprite_buffer
+    
+    fn scan_oam(&mut self) -> Vec<Sprite> {
+        vec![]
     }
 
     fn render_sprite(&mut self, sprite: Sprite) {
 
-        let mut tile_y = ((self.r.ly as i16 + sprite.y) % 8) as u16;
-        if sprite.y_flip != 0 {tile_y = 7 - tile_y}
-
-        let tile_num = sprite.tile_num;
-
-        let mut tile_addr: u16 = 0x8000 + (tile_num as u16) * 16;
-
-        tile_addr += 2 * tile_y;
-
-        for x in sprite.x..sprite.x+8 {    
-            let mut tile_x = x as u16 - sprite.x as u16;
-            if sprite.x_flip != 0 {tile_x = 7 - tile_x}
-            let shift = 7 - tile_x;
-
-            let data_low = (self.read(tile_addr) >> shift) & 1;
-            let data_high = (self.read(tile_addr + 1) >> shift) & 1;
-            let index = (self.r.ly) as usize * 160 + x as usize;
-            let should_draw = true; // TODO add some semi-accurate condition here
-            if should_draw {
-                self.backbuf()[index] =
-                    self.map_sprite_palette(sprite.pal, data_low | (data_high << 1));
-            }
-        }
-    
     }
+
 
     fn render_scanline(&mut self) { 
             if ((self.r.lcdc >> 7) & 1) == 0 {
                 return;
             }
+
             if (self.r.lcdc & 1) != 0 {
                 self.render_background(); 
-                //self.render_window();
-            }
+                if (self.r.lcdc >> 5) & 1 != 0 {
+                   //self.render_window();  
 
-            // println!("{:#b}", self.r.lcdc);
-            
-
-            if ((self.r.lcdc >> 1) & 1) != 0 {
-                let sprites = self.scan_oam();
-                for sprite in sprites {
-                    self.render_sprite(sprite);
                 }
             }
+
+            // if ((self.r.lcdc >> 1) & 1) != 0 {
+            //     let sprites = self.scan_oam();
+            //     for sprite in sprites {
+            //         self.render_sprite(sprite)
+            //     }
+            // }
     }
 }
